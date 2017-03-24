b3482193abe6b7427783992306fad353085a8b62
Subhojit <subhojit1992.mondal@gmail.com>
2017-03-24 22:06:11 +0530

    updated files added

--- contact-app-firebase/app/contacts/contacts.js
+++ contact-app-firebase/app/contacts/contacts.js
@@ -13,24 +13,73 @@ app.controller('contactsController', ['$rootScope', '$scope', '$firebaseArray', 
   	$scope.addFormShow = true;
   	$scope.editFormShow = false;
 
+  	// all contacts are not loaded
+  	$scope.tableloaded = true;
+
+  	// all contacts are loaded after
+  	$scope.contacts.$loaded().then(function(){
+  		$scope.tableloaded = false;
+  	});
+
 
 	// add contact function
 	$scope.addContact = function(){
 		console.log('Adding contact...');
 
-		// add in the contacts object
-		$scope.contacts.$add({
-			name: $scope.name,
-			email: $scope.email,
-			phone: $scope.phone
-		}).then(function(ref){
-			var id = ref.key;
-			console.log('Added contact ' + id);
-			// clear the form after adding the contact
-			$scope.name = '',
-			$scope.email = '',
-			$scope.phone = ''
+		// store the email and phone value in the variable
+		var email = $scope.email,
+			phone = $scope.phone;
+
+		// after content $loaded *firebase method
+		$scope.contacts.$loaded().then(function(items) {
+
+			$scope.items = items; // populated array
+			// console.log($scope.items);
+
+			// checking the vales if exist in the firebase database 
+			function checkingData(){
+				var result = false;
+				// each loop for checking each data
+				angular.forEach($scope.items, function (item) {
+					// console.log(item);
+					if(item.phone === phone){
+						result = true;
+					}
+				});
+				// each loop
+				// return the boolean value form that function
+				return result;
+			}
+
+			// add contact function for adding the contact in the DB
+			function addContact(){
+				// add function
+				$scope.contacts.$add({
+					name: $scope.name,
+					email: $scope.email,
+					phone: $scope.phone
+				}).then(function(ref){
+					var id = ref.key;
+					console.log('Added contact ' + id);
+					// clear the form after adding the contact
+					$scope.name = '',
+					$scope.email = '',
+					$scope.phone = ''
+				});
+			}
+
+			// after checking data form the firebase DB then 
+			// add form value as per you exucute
+			if(checkingData() === true){
+				// data is exist alert
+				alert('The phone number already in the database.');
+			}else{
+				// data adding function
+				addContact();
+			}
+
 		});
+		
 	}
   	
 
